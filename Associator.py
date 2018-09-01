fd57ea669071105287368d3d0d44b93087f9fcb1
McSteve Ezikeoha <mcsteveezikeoha@yahoo.com>
2018-09-01 16:22:34 -0400

    Update Associator.py

--- Associator.py
+++ Associator.py
@@ -14,11 +14,15 @@ class Associator:
         self.debugging = False
 
     def _hash(self, key):
+        # This is the heart of the system. 
+        # The algorithm that takes in a string and returns an index number from 0 up to the size of the primary arrays.
         total = (ord(key[0])+ord(key[-1]))*79
         value = total % self.size
         return value
 
     def _rehash(self, key):
+        # When there is a collision, we run a second algorithm to try a different slot in the primary.
+        # It returns an index number from 0 up to the size of the primary arrays.   
         pos = 0
         total = 0
         for x in key:
@@ -27,6 +31,7 @@ class Associator:
         return value
 
     def put(self, key, value):
+        # Put a key/value pair into the hashmap.
         h = self._hash(key)
         r = self._rehash(key)
         o = 0
@@ -56,6 +61,7 @@ class Associator:
         return True
 
     def get(self, key):
+        # Get the value associated with the given key, or None if it does not exist
         h = self._hash(key)
         r = self._rehash(key)
         o = 0
@@ -76,10 +82,15 @@ class Associator:
                     return self.ovflw_values[o]
         return None
 
-#   def delete(key):
-#       return
+'''
+    def delete(key):
+        # Remove the key/value pair if it exists (otherwise do nothing)
+        # If the key/value pair is in secondary, then leave a None to mark an empty slot
+        return
+'''
 
     def exists(self, key):
+        # Return Boolean after trying to find the key in the hashmap
         h = self._hash(key)
         r = self._hash(key)
         if self.prim_keys[h] == key:
@@ -87,11 +98,18 @@ class Associator:
         elif self.ovflw_keys[r] == key:
             return True
         else: return False
- 
-#   def find(value):
-#       return
+
+'''        
+    def find(value):
+        # Find the given value and return a list of keys that mapped into it.
+        return
+'''
 
     def stats(self):
+        # Return four ints: the size of the primary area,
+        #                   how many slots of the primary area are in use,
+        #                   the size of the secondary area, and
+        #                   how many slots of secondary are in use
         size = self.size
         prim_used = 0
         ovflw_used = 0
@@ -108,6 +126,7 @@ class Associator:
         return
 
     def debug(self):
+        # Turn on debugging, which shows the hashed value of every key
         self.debugging = True
         for key in self.prim_keys:
             if key != None:
@@ -130,6 +149,8 @@ class Associator:
         return s
             
     def print(self):
+        # Print the entire hashmap (not in any particular order).
+        # Show the key=value pairs, starting with primary and going to secondary.
         print("\nPRIMARY AREA")
         print("--------------------")
         count=0
@@ -161,19 +182,30 @@ class Associator:
         self.ovflw_values = Array.Array(size)
                 
     def _find_ovflow(self, key):
+        # Sequentially search the overflow area for the key.
+        # If not found, return -1.
+        # Otherwise, return the index where found.
         for i in range(len(self.ovflw_keys)):
             if self.ovflw_keys[i] == key:
                 return i
         return -1
 
     def _find_empty(self):
+        # Sequentially search the overflow area for the next space that is None.
+        # Return the index if you find an open slot.
+        # If not, then call _resize() and return to find the next empty slot.
         for i in range(len(self.ovflw_keys)):
             if self.ovflw_keys[i] == None:
                 return i
         return #supposed to return self._resize() but didn't do that method
 
-#   def _resize(self):
-#       return
-
-#   def _compact(self):
-#       return
+'''
+    def _resize(self):
+    # If you run out of room, expand the secondary area to be twice its original size, 
+    # copy all the existing secondary key/value pairs into the new version.
+        return
+    
+    def _compact(self):
+    # This squeezes out all the empty space from the overflow area and puts these "none" slots at the end.
+        return
+'''
